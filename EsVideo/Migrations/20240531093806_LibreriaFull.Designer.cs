550756e4e14e2498165948ee61cb63d2bf50612a
Andrea Martino <andrea.martino@isisfacchinetti.ed.it>
2024-06-03 10:01:03 +0200

    l amadre di tinoAggiungere i file di progetto.

--- EsVideo/Migrations/20240531093806_LibreriaFull.Designer.cs
+++ EsVideo/Migrations/20240531093806_LibreriaFull.Designer.cs
@@ -3,6 +3,7 @@ using EsVideo.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EsVideo.Migrations
 {
     [DbContext(typeof(ApllicatinDbContext))]
-    partial class ApllicatinDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240531093806_LibreriaFull")]
+    partial class LibreriaFull
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
